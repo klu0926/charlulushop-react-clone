fd4794ed232948f4ee3b8dc6df6968a5be5bca50
klu0926 <lukuoyu@gmail.com>
2024-03-22 10:36:16 +0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/charlulushop-react/',
   plugins: [react()],
   server: {
     port: 3001,
