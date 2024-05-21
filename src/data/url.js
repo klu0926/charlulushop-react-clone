8c24703a4a788931e8c30b1bb274b3e90f30b5aa
klu0926 <lukuoyu@gmail.com>
2024-05-21 10:49:40 +0800

    fix: url base to empty

--- src/data/url.js
+++ src/data/url.js
@@ -1,7 +1,8 @@
 const isLocal = window.location.hostname === 'localhost';
 const heroku = 'https://charlulu-shop-546c2a4689b9.herokuapp.com/api'
 const local = 'http://localhost:3000/api'
-const base = '/charlulushop-react'
+const base = ''
+//const base = '/charlulushop-react'
 
 const url = {
   server: isLocal ? local : heroku,
